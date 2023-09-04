f2a1a55d1ee86446237dd9c0d0cb89415694d4d6
Rastislav Madera <maderarasto@gmail.com>
2023-09-04 09:47:07 +0200

    Changed highlighting challenge item component.

--- src/components/ChallengeItem.tsx
+++ src/components/ChallengeItem.tsx
@@ -1,4 +1,4 @@
-import { useNavigate, useNavigation } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 
 type ChallengeItemProps = {
     title: string
@@ -20,11 +20,12 @@ export default function ChallengeItem({
     }
 
     return (
-        <div className="relative group w-[240px] h-[240px] border-[1px] border-solid border-gray-300 rounded-lg shadow-md cursor-pointer" onClick={handleClick}>
+        <div className="relative group w-[240px] h-[240px] border-[1px] border-solid border-gray-300 rounded-lg shadow-md cursor-pointer hover:ring-2 hover:ring-violet-200" onClick={handleClick}>
             <img className="w-full h-full rounded-lg" src={previewImage} alt={`Preview image of challenge ${title}`} />
-            <div className="absolute bottom-0 left-0 w-full py-4 px-2 bg-gradient-to-t from-gray-300 group-hover:from-indigo-200">
+            <div className="absolute inset-0 bg-gradient-to-t from-slate-100 group-hover:from-violet-100"></div>
+            <div className="absolute bottom-0 left-0 w-full py-4 px-2">
                 <h3 className="font-semibold">{title}</h3>
-                <span className="py-1 px-2 rounded-md text-xs bg-amber-400">{state}</span>
+                <span className="py-1 px-2 rounded-md text-xs bg-amber-300">{state}</span>
             </div>
         </div>
     )
--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -2,8 +2,8 @@ import { Outlet } from "react-router-dom";
 
 export default function Layout() {
     return (
-        <div className="flex items-center justify-center min-h-screen mx-auto bg-slate-300">
-            <div className="w-full max-w-6xl h-[768px] rounded-lg shadow-md bg-white">
+        <div className="flex items-center justify-center min-h-screen mx-auto bg-gray-100">
+            <div className="w-full max-w-6xl h-[768px] rounded-lg shadow-md bg-gray-50">
                 <Outlet />
             </div>
         </div>
