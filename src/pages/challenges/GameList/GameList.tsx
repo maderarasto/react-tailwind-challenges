61ce34641cd0cdb7e61c29ca04f68621be75f491
Rastislav Madera <maderarasto@gmail.com>
2023-09-20 08:54:42 +0200

    #GameList: Added a navigation component

--- src/pages/challenges/GameList/GameList.tsx
+++ src/pages/challenges/GameList/GameList.tsx
@@ -4,10 +4,12 @@ export default function GameListChallenge() {
     return (
         <div className="min-h-screen bg-stone-200">
             <header className="flex items-end h-48 bg-white shadow-md">
-                <div className="container mx-auto">
+                <div className="md:container mx-auto">
                     <Navigation />
                 </div>
             </header>
+            <div>
+            </div>
         </div>
     )
 }
--- src/pages/challenges/GameList/components/Navigation.tsx
+++ src/pages/challenges/GameList/components/Navigation.tsx
@@ -1,3 +1,5 @@
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { NavLink } from "react-router-dom"
 
 export default function Navigation() {
@@ -5,22 +7,27 @@ export default function Navigation() {
         <nav className={`flex justify-between h-16`}>
             <ul className="flex gap-12 text-xl text-gray-400">
                 <li className="flex items-center border-b-4 border-solid border-transparent">
-                    <NavLink to="">Home</NavLink>
+                    <NavLink to="" className="opacity-50 pointer-events-none">Home</NavLink>
                 </li>
                 <li className="flex items-center border-b-4 border-solid border-transparent">
-                    <NavLink to="">Apps</NavLink>
+                    <NavLink to="apps" className="opacity-50 pointer-events-none">Apps</NavLink>
                 </li>
                 <li className="flex items-center font-medium border-b-4 border-solid border-gray-300 text-indigo-400">
                     <NavLink to="">Games</NavLink>
                 </li>
                 <li className="flex items-center border-b-4 border-solid border-transparent">
-                    <NavLink to="">Music</NavLink>
+                    <NavLink to="" className="opacity-50 pointer-events-none">Music</NavLink>
                 </li>
                 <li className="flex items-center border-b-4 border-solid border-transparent">
-                    <NavLink to="">Movies & TV</NavLink>
+                    <NavLink to="" className="opacity-50 pointer-events-none">Movies & TV</NavLink>
                 </li>
             </ul>
-            <div>
+            <div className="flex items-center">
+                <div className="px-2 text-xl text-gray-400">
+                    <FontAwesomeIcon icon={faMagnifyingGlass} className="mr-2" />
+                    <label htmlFor="search-input"></label>
+                    <input type="text" id="search-input" placeholder="Search" className="w-40 p-2 outline-none placeholder:text-gray-400" />
+                </div>
             </div>
         </nav>
     )
