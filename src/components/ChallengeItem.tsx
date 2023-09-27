216f041384bfff348ecb25156e8cc9508c1b68d6
Rastislav Madera <maderarasto@gmail.com>
2023-09-28 01:17:27 +0200

    #General: Updated challenges.

--- src/components/ChallengeItem.tsx
+++ src/components/ChallengeItem.tsx
@@ -22,7 +22,7 @@ export default function ChallengeItem({
     }
 
     return (
-        <div className="group self-start rounded-lg bg-white shadow-lg cursor-pointer overflow-hidden" onClick={handleClick}>
+        <div className="group self-start rounded-lg bg-gray-200 shadow-lg cursor-pointer overflow-hidden" onClick={handleClick}>
             <img className="contrast-75 group-hover:contrast-100" src={previewImage} alt={`Preview image of challenge ${title}`} />
             <div className="p-4">
                 <h2 className="mb-1 font-bold text-lg">{title}</h2>
